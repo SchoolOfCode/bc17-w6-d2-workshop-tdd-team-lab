6f3078a00e8556bcf4ee0175e5fce2e012bbac6a
Liban Yusuf <lyus01306@gmail.com>
2024-08-06 12:34:16 +0100

    Ticket 2 nearly complete

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,42 @@
-export function calculateScrabbleScore(word) {}
+export function calculateScrabbleScore(word) {
+    if (word=== 'A'||'E'||'I'||'L'||'N'||'O'||'R'||'S'||'T'||'U' ){
+        return 1
+    }
+    else if (word=== 'D'||'G' ){
+        return 2
+    }
+  else if 
+}
+
+
+
+
+
+// | Letter | Score |
+// | ------ | ----- |
+// | A      | 1     |
+// | B      | 3     |
+// | C      | 3     |
+// | D      | 2     |
+// | E      | 1     |
+// | F      | 4     |
+// | G      | 2     |
+// | H      | 4     |
+// | I      | 1     |
+// | J      | 8     |
+// | K      | 5     |
+// | L      | 1     |
+// | M      | 3     |
+// | N      | 1     |
+// | O      | 1     |
+// | P      | 3     |
+// | Q      | 10    |
+// | R      | 1     |
+// | S      | 1     |
+// | T      | 1     |
+// | U      | 1     |
+// | V      | 4     |
+// | W      | 4     |
+// | X      | 8     |
+// | Y      | 4     |
+// | Z      | 10    |
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,6 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+import {test,expect} from 'vitest'
+
+test('calculate score for letter A',() =>{
+    expect(calculateScrabbleScore('A')).toBe(1)
+})
